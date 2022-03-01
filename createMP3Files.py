677d4610fbb1805967dc59232abe81c57bde0ed0
Rebecca Wilson <WilsonRL@users.noreply.github.com>
2022-03-01 14:49:00 -0500

    Update createMP3Files.py
    Test file to create MP3 files on RPi. Please read the notes for library installs and don't forget to connect the speaker to the Rpi  otherwise the files won't play.

--- createMP3Files.py
+++ createMP3Files.py
@@ -2,8 +2,16 @@
 # [1] https://www.geeksforgeeks.org/convert-text-speech-python/
 # [2] https://chrisrosser.net/posts/2020/04/06/using-sox-on-macos/
 
+# NOTES
+# In order for this file to play the audio files, you need to have the
+# speaker connected to the RPi (USB and audio jack)
+# You may need to install the following libraries:
 # pip install gTTS
+# on mac (assuming you use  homebrew):
 # brew install sox 
+# on RPi:
+# sudo apt-get install sox
+# sudo apt-get install libsox-fmt-mp3
 
 # Import libraries
 from gtts import gTTS
