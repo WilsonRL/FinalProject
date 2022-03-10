375eed0dc4406263d0041058b60b1a3403a775ec
Rebecca Wilson <WilsonRL@users.noreply.github.com>
2022-03-10 17:35:20 -0500

    Create secrets.py
    This file is for feather. You need to edit this file to include your ssid and password.

---
+++ FeatherFiles/secrets.py
@@ -0,0 +1,22 @@
+################################################################################
+# Rebecca Wilson
+# Last Modified: February 20
+# secrets.py
+#
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+#
+# REFERENCES
+# [1] https://learn.adafruit.com/magtag-twitter-display/circuitpython-internet-test
+# [2] https://learn.adafruit.com/adafruit-magtag-covid-vaccination-percent-tracker/getting-the-date-time
+################################################################################
+
+
+
+secrets = {
+    'ssid' : 'home_wifi_network',
+    'password' : 'wifi_password',
+    'aio_username' : 'my_adafruit_io_username',
+    'aio_key' : 'my_adafruit_io_key',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    }
