f38bd83ecfbf10077b2b4b516db28c41be03880e
Rebecca Wilson <WilsonRL@users.noreply.github.com>
2022-03-20 19:19:11 -0400

    Create personal.py

---
+++ server/personal.py
@@ -0,0 +1,11 @@
+# REFERENCES
+# [1] https://learn.adafruit.com/magtag-twitter-display/circuitpython-internet-test
+# [2] https://learn.adafruit.com/adafruit-magtag-covid-vaccination-percent-tracker/getting-the-date-time
+
+personalData = {
+    'ssid' : 'ssid goes here',
+    'password' : 'password goes here',
+    'aio_username' : 'username goes here',
+    'aio_key' : 'aio key goes here',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    }
